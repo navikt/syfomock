0a794403200f77c2ee9d539f7babc0ab2b156843
Martin Solheim <martin.solheim@me.com>
2019-05-08 10:03:36 +0200

    Refactor: Move opprett sykmelding to own file

--- src/Main.elm
+++ src/Main.elm
@@ -2,15 +2,11 @@ module Main exposing (main)
 
 import Browser
 import Browser.Navigation as Nav
-import Felleskomponenter exposing (skjemaElement)
 import Html exposing (..)
-import Html.Attributes exposing (class, style, type_, value)
-import Html.Events exposing (onInput, onSubmit)
-import Http exposing (post)
-import Json.Decode as Decode
-import Json.Encode as Encode
+import Html.Attributes exposing (class)
 import Model exposing (..)
 import NullstillBruker exposing (nullstillBruker, nullstillBrukerForm)
+import OpprettSykmelding exposing (lagSykmeldingBestilling, postNySykmelding, sykmeldingForm)
 import Url exposing (Url)
 
 
@@ -50,18 +46,6 @@ view model =
             ]
         ]
     }
-
-
-sykmeldingForm : Model -> Html Msg
-sykmeldingForm model =
-    form [ onSubmit SubmitOpprettSykmelding ]
-        [ div [ class "blokk-m" ]
-            [ skjemaElement "Fødselsnummer" "text" model.fnr Fnr
-            , skjemaElement "Startdato" "date" model.startDato StartDato
-            , skjemaElement "Sluttdato" "date" model.sluttDato SluttDato
-            ]
-        , button [ class "knapp knapp--hoved" ] [ text "Opprett sykmelding" ]
-        ]
 
 
 
@@ -116,67 +100,6 @@ update msg model =
 
         NoOp ->
             ( model, Cmd.none )
-
-
-postNySykmelding : SykmeldingBestilling -> Cmd Msg
-postNySykmelding bestilling =
-    let
-        body =
-            bestilling
-                |> sykmeldingBestillingEncoder
-                |> Http.jsonBody
-    in
-    post
-        { url = "https://httpbin.org/post"
-        , body = body
-        , expect = Http.expectJson SykmeldingSendt (Decode.list Decode.string)
-        }
-
-
-lagSykmeldingBestilling : Model -> SykmeldingBestilling
-lagSykmeldingBestilling model =
-    { fnr = model.fnr
-    , syketilfelleStartDato = model.startDato
-    , identdato = model.startDato
-    , utstedelsesdato = model.startDato
-    , smtype = "HUNDEREPROSENT"
-    , perioder =
-        [ { fom = model.startDato
-          , tom = model.sluttDato
-          , sykmeldingstype = HUNDREPROSENT
-          }
-        ]
-    , manglendeTilrettelegging = False
-    }
-
-
-sykmeldingBestillingEncoder : SykmeldingBestilling -> Encode.Value
-sykmeldingBestillingEncoder bestilling =
-    Encode.object
-        [ ( "fnr", Encode.string bestilling.fnr )
-        , ( "syketilfelleStartDato", Encode.string bestilling.syketilfelleStartDato )
-        , ( "identdato", Encode.string bestilling.identdato )
-        , ( "utstedelsesdato", Encode.string bestilling.utstedelsesdato )
-        , ( "smtype", Encode.string bestilling.smtype )
-        , ( "perioder", Encode.list sykmeldingsperiodeEncoder bestilling.perioder )
-        , ( "manglendeTilrettelegging", Encode.bool bestilling.manglendeTilrettelegging )
-        ]
-
-
-sykmeldingsperiodeEncoder : SykmeldingPeriode -> Encode.Value
-sykmeldingsperiodeEncoder periode =
-    Encode.object
-        [ ( "fom", Encode.string periode.fom )
-        , ( "tom", Encode.string periode.tom )
-        , ( "type", sykmeldingstypeEncoder periode.sykmeldingstype )
-        ]
-
-
-sykmeldingstypeEncoder : Sykemeldingstype -> Encode.Value
-sykmeldingstypeEncoder smtype =
-    case smtype of
-        _ ->
-            Encode.string "HUNDREPROSENT"
 
 
 
---
+++ src/OpprettSykmelding.elm
@@ -0,0 +1,83 @@
+module OpprettSykmelding exposing (lagSykmeldingBestilling, postNySykmelding, sykmeldingBestillingEncoder, sykmeldingForm, sykmeldingsperiodeEncoder, sykmeldingstypeEncoder)
+
+import Felleskomponenter exposing (skjemaElement)
+import Html exposing (Html, button, div, form, text)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onSubmit)
+import Http exposing (post)
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Model exposing (Model, Msg(..), Sykemeldingstype(..), SykmeldingBestilling, SykmeldingPeriode)
+
+
+sykmeldingForm : Model -> Html Msg
+sykmeldingForm model =
+    form [ onSubmit SubmitOpprettSykmelding ]
+        [ div [ class "blokk-m" ]
+            [ skjemaElement "Fødselsnummer" "text" model.fnr Fnr
+            , skjemaElement "Startdato" "date" model.startDato StartDato
+            , skjemaElement "Sluttdato" "date" model.sluttDato SluttDato
+            ]
+        , button [ class "knapp knapp--hoved" ] [ text "Opprett sykmelding" ]
+        ]
+
+
+postNySykmelding : SykmeldingBestilling -> Cmd Msg
+postNySykmelding bestilling =
+    let
+        body =
+            bestilling
+                |> sykmeldingBestillingEncoder
+                |> Http.jsonBody
+    in
+    post
+        { url = "https://httpbin.org/post"
+        , body = body
+        , expect = Http.expectJson SykmeldingSendt (Decode.list Decode.string)
+        }
+
+
+lagSykmeldingBestilling : Model -> SykmeldingBestilling
+lagSykmeldingBestilling model =
+    { fnr = model.fnr
+    , syketilfelleStartDato = model.startDato
+    , identdato = model.startDato
+    , utstedelsesdato = model.startDato
+    , smtype = "HUNDEREPROSENT"
+    , perioder =
+        [ { fom = model.startDato
+          , tom = model.sluttDato
+          , sykmeldingstype = HUNDREPROSENT
+          }
+        ]
+    , manglendeTilrettelegging = False
+    }
+
+
+sykmeldingBestillingEncoder : SykmeldingBestilling -> Encode.Value
+sykmeldingBestillingEncoder bestilling =
+    Encode.object
+        [ ( "fnr", Encode.string bestilling.fnr )
+        , ( "syketilfelleStartDato", Encode.string bestilling.syketilfelleStartDato )
+        , ( "identdato", Encode.string bestilling.identdato )
+        , ( "utstedelsesdato", Encode.string bestilling.utstedelsesdato )
+        , ( "smtype", Encode.string bestilling.smtype )
+        , ( "perioder", Encode.list sykmeldingsperiodeEncoder bestilling.perioder )
+        , ( "manglendeTilrettelegging", Encode.bool bestilling.manglendeTilrettelegging )
+        ]
+
+
+sykmeldingsperiodeEncoder : SykmeldingPeriode -> Encode.Value
+sykmeldingsperiodeEncoder periode =
+    Encode.object
+        [ ( "fom", Encode.string periode.fom )
+        , ( "tom", Encode.string periode.tom )
+        , ( "type", sykmeldingstypeEncoder periode.sykmeldingstype )
+        ]
+
+
+sykmeldingstypeEncoder : Sykemeldingstype -> Encode.Value
+sykmeldingstypeEncoder smtype =
+    case smtype of
+        _ ->
+            Encode.string "HUNDREPROSENT"
